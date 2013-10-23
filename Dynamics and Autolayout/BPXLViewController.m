7f708e157f25113ed05cc924ee3c98e2fcb39b87
Brandon Alexander <brandon@blackpixel.com>
2013-10-22 22:02:02 -0400

    Initial Commit

---
+++ Dynamics and Autolayout/BPXLViewController.m
@@ -0,0 +1,29 @@
+//
+//  BPXLViewController.m
+//  Dynamics and Autolayout
+//
+//  Created by Brandon Alexander on 10/22/13.
+//  Copyright (c) 2013 Black Pixel. All rights reserved.
+//
+
+#import "BPXLViewController.h"
+
+@interface BPXLViewController ()
+
+@end
+
+@implementation BPXLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
